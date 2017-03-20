7d33a3a3d573d7096e2f1016b0ec433de5227035
Matyas Buczko <whybuzz@gmail.com>
2017-03-20 10:23:31 +0000

    add swipe handling

--- backgrounds.js
+++ backgrounds.js
@@ -5,23 +5,33 @@ function generateDOM(html) {
     return parent.firstElementChild;
 }
 
-var backgrounds = [
-    'circular',
-    'croatia-sunset',
-    'rome-bridge',
-    'bath',
-    'feet',
-    'greece-sunset',
-    'krka',
-    'london',
-    'milan-green',
-    'milan-restaurant',
-    'sibenik-old-town',
-    'sibenik',
-    'singapore-avatar-2',
-];
-var backgroundPosition = 0;
+var backgrounds = isMobile
+    ? [
+        'circular',
+        'croatia-sunset',
+        'london',
+        'sibenik',
+        'singapore-avatar-2',
+    ]
+    : [
+        'circular',
+        'croatia-sunset',
+        'rome-bridge',
+        'bath',
+        'feet',
+        'greece-sunset',
+        'krka',
+        'london',
+        'milan-green',
+        'milan-restaurant',
+        'sibenik-old-town',
+        'sibenik',
+        'singapore-avatar-2',
+    ];
+
+var backgroundPosition = -1;
 var backgroundTimerPeriod = 12000;
+var LEFT = 2, RIGHT = 4;
 
 var $backgrounds = document.querySelector('#body-backgrounds');
 backgrounds.forEach(function(bg) {
@@ -30,14 +40,30 @@ backgrounds.forEach(function(bg) {
     );
 });
 $backgrounds.childNodes[0].className = 'active';
-function changeBackground() {
-    backgroundPosition++;
+var timerId = null;
+function changeBackground(direction) {
+    if(timerId) {
+        clearInterval(timerId);
+    }
+    if(direction === RIGHT) {
+        backgroundPosition--;
+    } else {
+        backgroundPosition++;
+    }
+
     if(backgroundPosition >= backgrounds.length) backgroundPosition = 0;
+    if(backgroundPosition < 0) backgroundPosition = backgrounds.length - 1;
 
     document.querySelectorAll('#body-backgrounds div').forEach(function(element) {
         element.className = '';
     });
     $backgrounds.childNodes[backgroundPosition].className = 'active';
-}
 
-window.setInterval(changeBackground, backgroundTimerPeriod);
+    timerId = setTimeout(changeBackground, backgroundTimerPeriod);
+}
+changeBackground();
+
+new Hammer(document.body)
+    .on('swipe', function(ev) {
+    	changeBackground(ev.direction);
+    });
