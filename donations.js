c3f71ccd1330ed54093d16565ac02233f3c19b6e
Matyas Buczko <whybuzz@gmail.com>
2017-03-27 22:20:20 +0100

    add donations container

---
+++ donations.js
@@ -0,0 +1,5 @@
+
+document.querySelector("#open-donations").onclick = function(event) {
+    event.preventDefault();
+    document.querySelector(".donation-container").className = "donation-container";
+};
