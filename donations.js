6b72479db3df6ba9aba13c4b591da5b1f3e5e0f2
Matyas Buczko <whybuzz@gmail.com>
2017-06-08 12:17:46 +0100

    make donations work again :)

--- donations.js
+++ donations.js
@@ -1,7 +1,7 @@
 
-// document.querySelector("#open-donations").onclick = function(event) {
-//     event.preventDefault();
-//     document.querySelectorAll(".donation-container").forEach(function(element) {
-//         element.className = "donation-container";
-//     });
-// };
+document.querySelector("#open-donations").onclick = function(event) {
+    event.preventDefault();
+    document.querySelectorAll(".donation-container").forEach(function(element) {
+        element.className = "donation-container";
+    });
+};
